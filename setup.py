31dbde92e6919e819a55196a8ceb5acfdbd596d4
Marc Rijken <marc@rijken.org>
2014-11-08 21:24:49 +0100

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
     open('CHANGES.txt').read())
 
 setup(name='pyramid_bowerstatic',
-      version='0.1',
+      version='0.2.dev0',
       description="BowerStatic integration for Pyramid",
       long_description=long_description,
       author="Marc Rijken",
